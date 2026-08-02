935a59a8a272e6aef83f13aeb988b8602acf00e9
Fadhel <fadhel.s.hashem@gmail.com>
2026-08-02 20:40:17 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+// src/App.jsx
+
+const App = () => {
+  return (
+    <main>
+  <h1>Hello world!</h1>;
+    </main>
+  )
+}
+
+export default App
